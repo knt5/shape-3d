63cadf8a9ca6e1982aab9b5342de7e3589cb7fe0
knt5 <knt5@users.noreply.github.com>
2016-07-03 23:02:16 +0900

    Remove noise

--- src/js/stage/datGui.js
+++ src/js/stage/datGui.js
@@ -2,8 +2,7 @@ const datGui = new dat.GUI();
 const config = {
 	sunDirection: 0.97 * Math.PI,
 	sunHeight: 220,
-	autoRatation: false,
-	
+	autoRatation: false
 };
 
 init();
@@ -19,6 +18,4 @@ function init() {
 	datGui.add(config, 'sunDirection', 0, Math.PI * 2);
 	datGui.add(config, 'sunHeight', 0, 500);
 	datGui.add(config, 'autoRatation');
-	
-	
 }
